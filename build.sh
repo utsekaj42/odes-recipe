f214dbb840f773f2dde7927ce18df75a63adb0f9
utsekaj42 <utsekaj42@users.noreply.github.com>
2017-05-24 15:56:20 +0200

    added scripts

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Add more build steps here, if they are necessary.
+$PYTHON setup.py install 
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
